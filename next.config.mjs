1f2a14f03950e72e06127a84d985bfea0d063aa0
“Jason <jasonrodalez@gmail.com>
2026-03-01 12:12:43 -0500

    fix: manifest y proxy transparente

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,12 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api-proxy/:path*',
+        // Esto captura la ruta que tú estás probando directamente
+        source: '/api/v1/:path*',
         destination: 'http://209.97.146.210/api/v1/:path*',
       },
       {
-        source: '/media-proxy/:path*',
+        source: '/media/:path*',
         destination: 'http://209.97.146.210/media/:path*',
       },
     ]
